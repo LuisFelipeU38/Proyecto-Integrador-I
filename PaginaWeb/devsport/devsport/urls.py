729a1bb8e5ad8c2e4e496dfc46389805cca341bf
Pradita777 <andres123prada@gmail.com>
2023-09-07 16:02:48 -0500

    Ya muestra imagenes siuuuu

--- PaginaWeb/devsport/devsport/urls.py
+++ PaginaWeb/devsport/devsport/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from talentos import views as talentosViews
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',talentosViews.home,name='home'),
-    path('about/', talentosViews.about),
+    path('talentos/', talentosViews.about),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
