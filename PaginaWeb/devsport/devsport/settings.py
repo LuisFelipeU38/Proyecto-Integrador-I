591e58e0119640b856fa25aadb77395c840a2b0b
Pradita777 <andres123prada@gmail.com>
2023-09-14 12:27:24 -0500

    Arreglamos errores

--- PaginaWeb/devsport/devsport/settings.py
+++ PaginaWeb/devsport/devsport/settings.py
@@ -41,9 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'talentos',
-    'crispy_forms',
-    'bootstrap4',
-
     ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
